7b66700ddc86bf04b38a46dc5824fda205d0111e
WakeupTsai <c20712983@gmail.com>
2018-07-31 09:59:10 +0000

    dpdk

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,11 @@ FROM golang:1.7.3 AS builder
 RUN go get github.com/prometheus/node_exporter
 COPY ./node-exporter/ /go/src/github.com/prometheus/node_exporter/collector
 WORKDIR /go/src/github.com/prometheus/node_exporter
-RUN make build test
+RUN make build
 
 # the final image
 FROM alpine:3.7
+RUN apk add pciutils
 COPY --from=builder /go/src/github.com/prometheus/node_exporter/node_exporter /bin/node_exporter
 EXPOSE      9100
 ENTRYPOINT  [ "/bin/node_exporter" ]
--- node-exporter/netdev_iface_linux.go
+++ node-exporter/netdev_iface_linux.go
@@ -2,9 +2,11 @@ package collector
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"strings"
 )
 
@@ -45,6 +47,7 @@ func getNetDevInfo() (map[string]map[string]string, error) {
 	// docker0 -> ../../devices/virtual/net/docker0
 	// eth0 -> ../../devices/pci0000:00/0000:00:01.0/virtio0/net/eth0
 	for iface, info := range netDev {
+		info["dpdk"] = "false"
 		devLink, _ := os.Readlink("/sys/class/net/" + iface)
 		if strings.Contains(devLink, "virtual") {
 			info["type"] = "virtual"
@@ -54,6 +57,44 @@ func getNetDevInfo() (map[string]map[string]string, error) {
 			// extract pci id
 			folderPath := strings.Split(devLink, "/")
 			info["pci_id"] = folderPath[4]
+		}
+	}
+
+	//get all interface dci_id (Slot column)
+	cmd := exec.Command("lspci", "-Dvmmn")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lines := strings.Split(out.String(), "\n")
+	dpdkCount := 0
+	for _, line := range lines {
+		tmp := strings.SplitN(line, ":", 2)
+		if tmp[0] == "Slot" {
+			pciID := strings.TrimPrefix(tmp[1], "\t")
+
+			// check whether this interface is dpdk, which driver is igb_uio
+			var subOut bytes.Buffer
+			subCmd := exec.Command("lspci", "-vmmks", pciID)
+			subCmd.Stdout = &subOut
+			err = subCmd.Run()
+			if err != nil {
+				log.Fatal(err)
+			}
+			if strings.Contains(subOut.String(), "igb_uio") == true {
+				dpdkName := fmt.Sprintf("dpdk%v", dpdkCount)
+				dpdkCount = dpdkCount + 1
+
+				info := map[string]string{}
+				netDev[dpdkName] = info
+				netDev[dpdkName]["dpdk"] = "true"
+				netDev[dpdkName]["default"] = "false"
+				netDev[dpdkName]["pci_id"] = pciID
+				netDev[dpdkName]["type"] = "virtual"
+			}
 		}
 	}
 
